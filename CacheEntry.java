41067a9c2473def11e1c6a561d6aea3a2699f2ee
Aalok Kumar Yadav <150387936+aalok2006@users.noreply.github.com>
2025-04-21 11:49:55 +0530

    Create CacheEntry.java

---
+++ CacheEntry.java
@@ -0,0 +1,10 @@
+// No imports needed for this simple class
+class CacheEntry {
+    String data;
+    long lastAccess;
+
+    CacheEntry(String data, long lastAccess) {
+        this.data = data;
+        this.lastAccess = lastAccess;
+    }
+}
